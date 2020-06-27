048017f142d94bf02f9b71a62f995c899aaa0ebc
Bhirahaspathi <bhirahaspathi@gmail.com>
2020-06-27 16:22:13 +0530

    Updates

---
+++ precache-manifest.eb87766f8632e1544e99de6c76ab40b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5b3ce2b602a778172b1b890fb4d3ac",
+    "url": "/poolcarapp/index.html"
+  },
+  {
+    "revision": "66d3306de01482de58ae",
+    "url": "/poolcarapp/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "631db49114e357009bb3",
+    "url": "/poolcarapp/static/css/main.15f2c2a3.chunk.css"
+  },
+  {
+    "revision": "66d3306de01482de58ae",
+    "url": "/poolcarapp/static/js/2.afc6997c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/poolcarapp/static/js/2.afc6997c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631db49114e357009bb3",
+    "url": "/poolcarapp/static/js/main.6ceb5995.chunk.js"
+  },
+  {
+    "revision": "1c29c475166447ba4605",
+    "url": "/poolcarapp/static/js/runtime-main.13ca8d5a.js"
+  },
+  {
+    "revision": "624e87743ab78ccc5dd318956608ea37",
+    "url": "/poolcarapp/static/media/image.624e8774.jpg"
+  }
+]);
